a33c0f2492777dc8906e3ad954d9d838c984ff0e
Ricardo Gallo <ricardo.gallo@rydevinc.com>
2024-11-15 11:52:12 -0300

    Format LAB 5 testbench

--- labs/lab5_1/src/cnt_tb.sv
+++ labs/lab5_1/src/cnt_tb.sv
@@ -1,32 +1,33 @@
 // `timescale 1ns/1ps
 module cnt_tb;
-   logic clk;
 
-   import definitions_pkg::*; // Defintions package imported
+  logic clk;
+
+  import definitions_pkg::*; // Defintions package imported
 
   // TB Clock Generator used to provide the design
   // with a clock -> here half_period = 10ns => 50 MHz
   always #10 clk = ~clk;
 
-// Instantiate the interface here
+  // Instantiate the interface here
   ?? //Interface is instantiated
 
-// connect the counter here
- 
- ??  counter_ud  c0 ( 	.clk 		(??),
-                  	.rstn 		(??),
-                  	.load 		(??),
-                  	.load_en 	(??),
-                  	.down 		(??),
-                  	.rollover (??),
-                  	.count 		(??));
+  // connect the counter here
+
+ ??  counter_ud  c0 ( .i_clk      (??),
+                      .i_rstn     (??),
+                      .i_load     (??),
+                      .i_load_en  (??),
+                      .i_down     (??),
+                      .o_rollover (??),
+                      .o_count    (??));
 
   initial begin
     bit load_en, down;
     bit [3:0] load;
 
     $monitor("[%0t] down=%0b load_en=%0b load=0x%0h count=0x%0h rollover=%0b",
-    	$time, cnt_if0.down, cnt_if0.load_en, cnt_if0.load, cnt_if0.count, cnt_if0.rollover);
+      $time, cnt_if0.down, cnt_if0.load_en, cnt_if0.load, cnt_if0.count, cnt_if0.rollover);
 
     // Initialize testbench variables
     clk = 0;
@@ -57,4 +58,5 @@ module cnt_tb;
     repeat(5) @ (posedge clk);
     $finish;
   end
+
 endmodule
